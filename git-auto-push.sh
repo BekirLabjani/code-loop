5ffbf971288087342a8e6c73f0050a22a328e93e
BekirLabjani <business.life.bekir@gmail.com>
2025-08-05 21:16:01 +0200

    Update 0.001

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -6,10 +6,9 @@ ng build --configuration=production
 
 echo 🟢 Build abgeschlossen. Git-Vorgang beginnt...
 
-git remote set-url origin git@github.com:BekirLabjani/go-taxi-web-page.git
-
+git remote set-url origin git@github.com:BekirLabjani/code-loop.git
 git add .
-git commit -m "Update 0.1"
+git commit -m "Update 0.001"
 git push origin main
 
 echo ✅ Fertig!
