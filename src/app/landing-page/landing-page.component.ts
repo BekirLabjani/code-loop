f0364f518ee89f122a0420ca4d529bf7588f0183
BekirLabjani <business.life.bekir@gmail.com>
2025-08-07 23:06:33 +0200

    Update 0.002

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
+import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, MainComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
