0d6119ac2f1b8e1456da5eaac79d2ee495101dcf
BekirLabjani <business.life.bekir@gmail.com>
2025-08-11 22:09:55 +0200

    Update 0.002

--- src/app/landing-page/main/main.component.ts
+++ src/app/landing-page/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ShowRoomComponent } from '../show-room/show-room.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ShowRoomComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
